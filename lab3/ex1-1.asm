d91e1046c86ab72e8dc1eee7431888b41f352293
Matheus Bittencourt <matheuspb2005@hotmail.com>
2016-09-22 16:44:57 -0300

    Laboratory 3

---
+++ lab3/ex1-1.asm
@@ -0,0 +1,23 @@
+.data
+_save: .word 6, 6, 6, 6, 7, 5, 6, 0, 6
+_k: .word 6
+.text
+.globl main
+main: # inicialização
+	add $s3, $zero, $zero
+	la $s5, _k
+	lw $s5, 0($s5)
+	la $s6, _save
+
+Loop: # corpo do laço
+	sll $t1,$s3,2
+	add $t1, $t1, $s6
+	lw $t0, 0($t1)
+	bne $t0, $s5, Exit
+	addi $s3, $s3, 1
+	j Loop
+Exit:
+# rotina para imprimir inteiro no console
+	addi $v0,$zero, 1
+	add $a0,$zero, $s3
+	syscall
---
+++ lab3/ex2-2.asm
@@ -0,0 +1,33 @@
+.data
+_save: .word 99999, 9, 6, 6, 6, 6, 6, 6, 6, 6, 6
+_k: .word 6
+_error: .asciiz "Index Out of Bounds Exception"
+
+.text
+.globl main
+main: # inicialização
+	add $s3, $zero, $zero
+	la $s5, _k
+	lw $s5, 0($s5)
+	la $s6, _save
+	lw $t2, 4($s6)
+
+Loop: # corpo do laço
+	sltu $t0, $s3, $t2
+	beq $t0, $zero, IndexOutOfBounds
+	sll $t1, $s3, 2
+	add $t1, $t1, $s6
+	lw $t0, 8($t1)
+	bne $t0, $s5, Exit
+	addi $s3, $s3, 1
+	j Loop
+Exit:
+	addi $v0,$zero, 1
+	add $a0,$zero, $s3
+	syscall
+	j End
+IndexOutOfBounds:
+	addi $v0,$zero, 4
+	la $a0,_error
+	syscall
+End:
